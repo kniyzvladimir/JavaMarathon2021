54a9c1161aae9ee0620242b180dd5648faceabb6
Volodymir Shpachnko <kniyzvladimir@gmail.com>
2021-01-20 22:57:49 +0200

    day 1/2

--- src/main/java/day1/Task6.java
+++ src/main/java/day1/Task6.java
@@ -2,6 +2,12 @@ package day1;
 
 public class Task6 {
     public static void main(String[] args) {
+        int k = 7;
+        int proizvedenie;
+        for(int i = 1; i <= 9; i++){
+            proizvedenie = i * k;
+            System.out.println(i + " x " + k + " = " + proizvedenie);
+        }
 
     }
 }

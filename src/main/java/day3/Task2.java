691f243c484fd933e418e85f8b7f872ebb8fec31
Volodymir Shpachnko <kniyzvladimir@gmail.com>
2021-01-22 23:37:46 +0200

    day 3

--- src/main/java/day3/Task2.java
+++ src/main/java/day3/Task2.java
@@ -1,7 +1,18 @@
 package day3;
 
+import java.util.Scanner;
+
 public class Task2 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        while(true){
+            double delimoe = scanner.nextDouble();
+            double delitel = scanner.nextDouble();
+            if (delitel == 0){
+                break;
+            }
+            System.out.println(delimoe / delitel);
+        }
 
     }
 }
--- src/main/java/day3/Task3.java
+++ src/main/java/day3/Task3.java
@@ -1,7 +1,19 @@
 package day3;
 
+import java.util.Scanner;
+
 public class Task3 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        for (int i = 0; i < 5; i++){
+            double delimoe = scanner.nextDouble();
+            double delitel = scanner.nextDouble();
+            if (delitel == 0){
+                System.out.println("Деление на 0");
+                continue;
+            }
+            System.out.println(delimoe / delitel);
+        }
 
     }
 }

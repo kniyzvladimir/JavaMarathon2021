020dc46f2e474728862cc253086b3ac4acbec83e
Volodymir Shpachnko <kniyzvladimir@gmail.com>
2021-01-26 23:27:53 +0200

    day 7

--- src/main/java/day7/Task1.java
+++ src/main/java/day7/Task1.java
@@ -2,6 +2,67 @@ package day7;
 
 public class Task1 {
     public static void main(String[] args) {
+        Airplane a1 = new Airplane("Antonov", 2020, 100, 7000000, "a1");
+        Airplane a2 = new Airplane("Boing", 2019, 70, 3000000, "a2");
 
+        Airplane.compareAirplanes(a1, a2);
+    }
+}
+
+class Airplane {
+    private String producer;
+    private int year;
+    private int length;
+    private int weight;
+    private int fuel;
+    private String objName;
+
+    public void setProducer(String producer) {
+        this.producer = producer;
+    }
+    public static void compareAirplanes(Airplane a1, Airplane a2){
+        if (a1.length == a2.length){
+            System.out.println("Длина самолётов равна");
+        } else if (a1.length > a2.length){
+            System.out.printf("Самолёт %s длинее чем самолёт %s", a1.objName, a2.objName);
+        } else {
+            System.out.printf("Самолёт %s длинее чем самолёт %s", a2.objName, a1.objName);
+        }
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public void setFuel(int fuel) {
+        this.fuel = fuel;
+    }
+
+    public void fillUp(int n) {
+        this.fuel += n;
+    }
+
+    public int getFuel() {
+        return fuel;
+    }
+
+    public void info() {
+        System.out.printf("Изготовитель: %s , год выпуска: %d , длина: %d, вес: %d, количество топлива в баке: %d", producer, year, length, weight, fuel);
+    }
+
+    public Airplane(String producer, int year, int length, int weight, String objName) {
+        this.producer = producer;
+        this.year = year;
+        this.weight = weight;
+        this.length = length;
+        this.objName = objName;
     }
 }
--- src/main/java/day7/Task2.java
+++ src/main/java/day7/Task2.java
@@ -1,7 +1,59 @@
 package day7;
 
+import java.util.Random;
+
 public class Task2 {
     public static void main(String[] args) {
+        Random random = new Random();
+        int randInt = random.nextInt(11) + 90;
+
+        Player p1 = new Player(randInt);
+        Player p2 = new Player(randInt);
+        Player p3 = new Player(randInt);
+        Player p4 = new Player(randInt);
+        Player p5 = new Player(randInt);
+        Player p6 = new Player(randInt);
+        Player p7 = new Player(randInt);
+
+        while(p1.getStamina() > 0){
+            p1.run();
+        }
+        Player.info();
+
 
     }
+}
+class Player{
+    private int stamina;
+    Player(int stamina){
+        this.stamina = stamina;
+        if (countPlayers < 6){
+            countPlayers++;
+        }
+    }
+
+    private final int MAX_STAMINA = 100;
+    private final int MIN_STAMINA = 0;
+    private static int countPlayers = 0;
+
+    public int getStamina(){
+        return stamina;
+    }
+    public static int getCountPlayers(){
+        return countPlayers;
+    }
+    public void run(){
+        stamina--;
+        if (stamina == MIN_STAMINA){
+            System.out.println("Игрок покинул поле");
+            countPlayers -= 1;
+        }
+    }
+    public static void info(){
+        if (countPlayers < 6){
+            System.out.println("Команды не полные, количество свободных мест на поле: " + (6 - countPlayers));
+        }else System.out.println("На поле нет свободных мес");
+    }
+
+
 }

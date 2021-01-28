55f634a3e8d5c1d4dd8fa2a368e36e3428543bde
Volodymir Shpachnko <kniyzvladimir@gmail.com>
2021-01-29 01:29:53 +0200

    day 9

--- src/main/java/day9/Task1/Task1.java
+++ src/main/java/day9/Task1/Task1.java
@@ -2,6 +2,62 @@ package day9.Task1;
 
 public class Task1 {
     public static void main(String[] args) {
+        Student s1 = new Student("Vasilyi", "k1");
+        Teacher t1 = new Teacher("Ivan", "Physics");
 
+        System.out.println(s1.getGroupName());
+        System.out.println(t1.getSubjectName());
+
+        s1.printInfo();
+        t1.printInfo();
+
+    }
+}
+
+abstract class Human {
+    private String name;
+    Human(String name){
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void printInfo(){
+        System.out.printf("Этот человек с именем %s", name);
+    }
+}
+
+class Student extends Human{
+    private String groupName;
+
+    public Student(String name, String groupName){
+        super(name);
+        this.groupName = groupName;
+    }
+
+    public String getGroupName() {
+        return groupName;
+    }
+    public void printInfo(){
+        System.out.printf("Этот человек с именем %s\n", super.getName());
+        System.out.printf("Этот студент с именем %s\n", super.getName());
+    }
+}
+class Teacher extends Human{
+    private String subjectName;
+    public Teacher(String name, String subjectName){
+        super(name);
+        this.subjectName = subjectName;
+    }
+
+    public String getSubjectName() {
+        return subjectName;
+    }
+
+    public void printInfo(){
+        System.out.printf("Этот человек с именем %s\n", super.getName());
+        System.out.printf("Этот преподаватель с именем %s\n", super.getName());
     }
 }
--- src/main/java/day9/Task2/TestFigures.java
+++ src/main/java/day9/Task2/TestFigures.java
@@ -2,6 +2,103 @@ package day9.Task2;
 
 public class TestFigures {
     public static void main(String[] args) {
+        Figure[] figures = {
+                new Triangle(10, 10, 10, "Red"),
+                new Triangle(10, 20, 30, "Green"),
+                new Triangle(10, 20, 15, "Red"),
+                new Rectangle(5, 10, "Red"),
+                new Rectangle(40, 15, "Orange"),
+                new Circle(4, "Red"),
+                new Circle(10, "Red"),
+                new Circle(5, "Blue")
+        };
+        System.out.printf("%.2f \n", calculateRedPerimeter(figures));
+        System.out.printf("%.2f \n", calculateRedArea(figures));
+    }
 
+    public static double calculateRedPerimeter(Figure[] figures){
+        double sumPerimeter = 0;
+        for (Figure f: figures){
+            if (f.getColor() == "Red"){
+                sumPerimeter += f.perimeter();
+            }
+        }
+        return sumPerimeter;
+    }
+
+    public static double calculateRedArea(Figure[] figures){
+        double sumArea = 0;
+        for (Figure f: figures){
+            if (f.getColor() == "Red"){
+                sumArea += f.area();
+            }
+        }
+        return sumArea;
+    }
+}
+abstract class Figure{
+    private String color;
+    Figure(String color){
+        this.color = color;
+    }
+    public String getColor() {
+        return color;
+    }
+    public abstract double area();
+    public abstract double perimeter();
+
+}
+class Circle extends Figure{
+    static final double PI = 3.14159265359;
+    private double radius;
+    public Circle(double radius, String color){
+        super(color);
+        this.radius = radius;
+    }
+    public double area(){
+        return PI * radius * radius;
+    }
+
+    public double perimeter(){
+        return 2 * PI * radius;
+    }
+}
+
+class Rectangle extends Figure{
+    private double width;
+    private double length;
+
+    public Rectangle (double width, double length, String color){
+        super(color);
+        this.width = width;
+        this.length = length;
+    }
+    public double area(){
+        return width * length;
+    }
+
+    public double perimeter(){
+        return (width + length) * 2;
+    }
+}
+
+class Triangle extends Figure{
+    private double a;
+    private double b;
+    private double c;
+
+    Triangle(double a, double b, double c, String color){
+        super(color);
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+    public double area(){
+        double p = (a + b + c) / 2;
+        return Math.sqrt(p * (p - a) * (p - b) * (p - c));
+    }
+
+    public double perimeter(){
+        return a + b + c;
     }
 }
